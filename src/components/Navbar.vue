9a9e4eca5b7dcbbd73ee3d1c1a9db21d8a778bf3
Snehal Reddy <syelmat@gmail.com>
2020-03-22 17:45:18 +0530

    initial commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,9 @@
+<template>
+  <nav>
+    <div class="nav-wrapper blue">
+      <div class="container">
+        <router-link to="/" class="brand-logo">VueEpman</router-link>
+      </div>
+    </div>
+  </nav>
+</template>
---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyB0-NnTVzBWyVUlMMb37WLCYiUVCKTwvfg",
+    authDomain: "vueepman.firebaseapp.com",
+    databaseURL: "https://vueepman.firebaseio.com",
+    projectId: "vueepman",
+    storageBucket: "vueepman.appspot.com",
+    messagingSenderId: "943135555524",
+    appId: "1:943135555524:web:40d58f32eac3ceac6fae91"
+}
