aae65719de2c0073949b25a821c7f2f8dfa58c84
Snehal Reddy <syelmat@gmail.com>
2020-03-29 18:01:37 +0530

    Delete firebaseConfig.js

--- src/components/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export default {
-    apiKey: "AIzaSyB0-NnTVzBWyVUlMMb37WLCYiUVCKTwvfg",
-    authDomain: "vueepman.firebaseapp.com",
-    databaseURL: "https://vueepman.firebaseio.com",
-    projectId: "vueepman",
-    storageBucket: "vueepman.appspot.com",
-    messagingSenderId: "943135555524",
-    appId: "1:943135555524:web:40d58f32eac3ceac6fae91"
-}
